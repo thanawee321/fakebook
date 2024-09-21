40d902f83bf0eccef284fdd3e5f17995b804197e
thanawee321 <53590032+thanawee321@users.noreply.github.com>
2024-09-21 09:19:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function fakebook() {
+
+    window.location.href = "http://www.facebook.com";
+
+}
+
+function forgot(){
+
+    window.location.href = "https://www.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0"
+}
